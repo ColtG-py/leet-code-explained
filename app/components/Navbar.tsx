0273375dfd4248794fe9173b253331d757151d9d
ColtG-py <80473907+ColtG-py@users.noreply.github.com>
2023-08-02 17:59:57 -0400

    adjusting navbar size

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { robotoMono } from "../fonts"
 
 export default function Navbar() {
     return (
-        <nav className="bg-slate-680 p-4 sticky top-0 drop-shadow-xl z-10">
+        <nav className="bg-slate-680 p-4 top-0 drop-shadow-xl z-10">
             <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
                 <h1 className={`${robotoMono.className} underlinetext-3xl font-extrabold grid place-content-center mb-2 md:mb-0 text-2xl font-bold grid place-content-center mb-2 md:mb-0`} >
                     <Link href="/" className="text-white-700 text-white/90 no-underline hover:text-white">
