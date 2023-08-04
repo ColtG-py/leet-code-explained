a3e081ca8e1825a292b16252d8fe90d4632c4760
ColtG-py <80473907+ColtG-py@users.noreply.github.com>
2023-08-04 18:28:41 -0400

    Massive list item overhaul

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -3,7 +3,6 @@ import path from 'path'
 import matter from 'gray-matter'
 import { remark } from 'remark'
 import html from 'remark-html'
-import Post from "../types/types"
 
 const postsDirectory = path.join(process.cwd(), 'posts')
 
