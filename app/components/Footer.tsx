b85e627d743ba329cf4192b90173d72220675502
ColtG-py <80473907+ColtG-py@users.noreply.github.com>
2023-08-06 13:12:25 -0400

    playing with format

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,13 +7,13 @@ export default function Footer() {
     return (
         <footer className="mt-2">
             <nav className="bg-slate-800">
-                <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
+                <div className="prose prose-xl mx-auto flex-row">
                     <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white lg:text-5xl">
                         <Subscribe />
-                    </div>
-                    <Link className="text-white/90 hover:text-white" href="https://ko-fi.com/coltgainey">     
+                    <Link className="text-white/90 hover:text-white ml-auto" href="https://ko-fi.com/coltgainey">     
                         <FaCoffee />
                     </Link>
+                    </div>  
                 </div>              
             </nav>
         </footer>
