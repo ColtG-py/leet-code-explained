caeb9db398724d79f4afb2ae07138b969bb32cf4
Colt Gainey <Colt.Gainey@gmail.com>
2024-07-24 12:00:09 -0400

    changing image

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -5,10 +5,10 @@ export default function MyProfilePic() {
         <section className="w-full mx-auto mb-20">
             <Image
                 className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
-                src="/images/websitepic.jpg"
+                src="/images/explainedengineering.png"
                 width={200}
                 height={200}
-                alt="Colt"
+                alt="explained engineering."
                 priority={true}
             />
         </section>
